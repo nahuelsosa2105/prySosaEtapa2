2d8fa4f5613bae43753aa11aa830aab7339061f1
nahuelsosa2105 <nahuelsosa2105@gmail.com>
2024-04-30 21:27:44 -0300

    Etapa 2 terminada Generacion de imagenes por codigo, por medio de un boton en la interfaz

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -28,21 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCrear = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.BackColor = System.Drawing.Color.Navy;
+            this.btnCrear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.ForeColor = System.Drawing.Color.White;
+            this.btnCrear.Location = new System.Drawing.Point(269, 407);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(237, 62);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "CREAR";
+            this.btnCrear.UseVisualStyleBackColor = false;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(775, 509);
+            this.Controls.Add(this.btnCrear);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CREAR OBJETOS POR CODIGO";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCrear;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -16,5 +16,61 @@ namespace prySosaEtapa2
         {
             InitializeComponent();
         }
+        clsVehiculo objAuto;
+        clsVehiculo objAvion;
+        clsVehiculo objBarco;
+        int cont = 1;
+
+        bool Barco = false;
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+           //Random rnd = new Random();
+            //int numAleatorio = rnd.Next(1,3);
+
+            
+            
+            switch (cont)
+            {
+                case 1:
+                    objAuto.CrearAuto();
+                    objAuto.pctAuto.Location = new Point(100, 50);
+                    Controls.Add(objAuto.pctAuto);
+                    cont++;
+                    if(Barco == true)
+                    {
+                        objBarco.pctBarco.Dispose();
+                    }
+                break;
+
+                case 2:
+                    objAvion.CrearAvion();
+                    objAvion.pctAvion.Location = new Point(300, 50);
+                    Controls.Add(objAvion.pctAvion);
+                    objAuto.pctAuto.Dispose();
+                    cont++;
+                break;
+
+                case 3:
+                    objBarco.CrearBarco();
+                    objBarco.pctBarco.Location = new Point(500, 50);
+                    Controls.Add(objBarco.pctBarco);
+                    objAvion.pctAvion.Dispose();
+                    cont++;
+                    cont = 1;
+                    Barco = true;
+                break;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            objAuto = new clsVehiculo();
+            objAvion = new clsVehiculo();
+            objBarco = new clsVehiculo();
+
+            objAuto.CrearAuto();
+            objAvion.CrearAvion();
+            objBarco.CrearBarco();
+        }
     }
 }
